d71eceb45070674140d8f76fe5f54d74f123f110
ivanslavkomatic <48771316+ivanslavkomatic@users.noreply.github.com>
2020-06-23 23:53:37 +0200

    Implemented a grid draw. Next up is insert the sockets, and that is it for board drawing.

---
+++ Morris/Morris/GridMaker.h
@@ -0,0 +1,156 @@
+#pragma once
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+//Grid could've been made in one massive stack,
+//but when it comes to drawing morris board, it gets unnecesarily confusing
+//considering, it has to be drawn in one stroke
+class Gridmaker {
+	public:
+		//Outer Square
+		VertexArray generateGridOuter() {
+			VertexArray square(LineStrip, 5);
+			square[0].position = Vector2f(100.f, 100.f);
+			square[1].position = Vector2f(800.f, 100.f);
+			square[2].position = Vector2f(800.f, 800.f);
+			square[3].position = Vector2f(100.f, 800.f);
+			square[4].position = Vector2f(100.f, 100.f);
+
+			square[0].color = Color::Red;
+			square[1].color = Color::Red;
+			square[2].color = Color::Red;
+			square[3].color = Color::Red;
+			square[4].color = Color::Red;
+
+			return square;
+		}
+		//Middle Square
+		VertexArray generateGridMid() {
+			VertexArray square(LineStrip, 5);
+			square[0].position = Vector2f(200.f, 200.f);
+			square[1].position = Vector2f(700.f, 200.f);
+			square[2].position = Vector2f(700.f, 700.f);
+			square[3].position = Vector2f(200.f, 700.f);
+			square[4].position = Vector2f(200.f, 200.f);
+
+			square[0].color = Color::Red;
+			square[1].color = Color::Red;
+			square[2].color = Color::Red;
+			square[3].color = Color::Red;
+			square[4].color = Color::Red;
+
+			return square;
+		}
+		//Inner Square
+		VertexArray generateGridInner() {
+			VertexArray square(LineStrip, 5);
+			square[0].position = Vector2f(300.f, 300.f);
+			square[1].position = Vector2f(600.f, 300.f);
+			square[2].position = Vector2f(600.f, 600.f);
+			square[3].position = Vector2f(300.f, 600.f);
+			square[4].position = Vector2f(300.f, 300.f);
+
+			square[0].color = Color::Red;
+			square[1].color = Color::Red;
+			square[2].color = Color::Red;
+			square[3].color = Color::Red;
+			square[4].color = Color::Red;
+
+			return square;
+		}
+		//Linestrips that will slash halves, a circle had to be made
+		//to achieve it in one stroke
+		VertexArray generateGridSlicers() {
+			VertexArray square(LineStrip, 15);
+			square[0].position = Vector2f(450.f, 100.f);
+			square[1].position = Vector2f(450.f, 200.f);
+			square[2].position = Vector2f(450.f, 300.f);
+			square[3].position = Vector2f(300.f, 300.f);
+			square[4].position = Vector2f(300.f, 450.f);
+			square[5].position = Vector2f(200.f, 450.f);
+			square[6].position = Vector2f(100.f, 450.f);
+			square[7].position = Vector2f(100.f, 800.f);
+			square[8].position = Vector2f(450.f, 800.f);
+			square[9].position = Vector2f(450.f, 700.f);
+			square[10].position = Vector2f(450.f, 600.f);
+			square[11].position = Vector2f(600.f, 600.f);
+			square[12].position = Vector2f(600.f, 450.f);
+			square[13].position = Vector2f(700.f, 450.f);
+			square[14].position = Vector2f(800.f, 450.f);
+
+			square[0].color = Color::Red;
+			square[1].color = Color::Red;
+			square[2].color = Color::Red;
+			square[3].color = Color::Red;
+			square[4].color = Color::Red;
+			square[5].color = Color::Red;
+			square[6].color = Color::Red;
+			square[7].color = Color::Red;
+			square[8].color = Color::Red;
+			square[9].color = Color::Red;
+			square[10].color = Color::Red;
+			square[11].color = Color::Red;
+			square[12].color = Color::Red;
+			square[13].color = Color::Red;
+			square[14].color = Color::Red;
+
+			return square;
+		}
+};
+
+/*
+Morris points
+	Outer
+			square[0].position = Vector2f(100.f, 100.f);
+			square[1].position = Vector2f(800.f, 100.f);
+			square[2].position = Vector2f(800.f, 800.f);
+			square[3].position = Vector2f(100.f, 800.f);
+	Mid
+			square[0].position = Vector2f(200.f, 200.f);
+			square[1].position = Vector2f(700.f, 200.f);
+			square[2].position = Vector2f(700.f, 700.f);
+			square[3].position = Vector2f(200.f, 700.f);
+	Inner
+			square[0].position = Vector2f(300.f, 300.f);
+			square[1].position = Vector2f(600.f, 300.f);
+			square[2].position = Vector2f(600.f, 600.f);
+			square[3].position = Vector2f(300.f, 600.f);
+	Slicers
+			square[0].position = Vector2f(450.f, 100.f);
+			square[1].position = Vector2f(450.f, 200.f);
+			square[2].position = Vector2f(450.f, 300.f);
+			square[4].position = Vector2f(300.f, 450.f);
+			square[5].position = Vector2f(200.f, 450.f);
+			square[6].position = Vector2f(100.f, 450.f);
+			square[8].position = Vector2f(450.f, 800.f);
+			square[9].position = Vector2f(450.f, 700.f);
+			square[10].position = Vector2f(450.f, 600.f);
+			square[12].position = Vector2f(600.f, 450.f);
+			square[13].position = Vector2f(700.f, 450.f);
+			square[14].position = Vector2f(800.f, 450.f);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
--- Morris/Morris/Morris.cpp
+++ Morris/Morris/Morris.cpp
@@ -1,34 +1,19 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-#include <SFML/GridMaker.h>
+#include "GridMaker.h"
 using namespace std;
 using namespace sf;
 
 int main()
 {
 	
-	RenderWindow window(VideoMode(1024, 768), "Nine Men's Morris", (Style::Resize,Style::Close));
-	RectangleShape background(Vector2f(800.f,600.f));
-	background.setPosition(112.f, 84.f);
-	/*Texture texture;
-	texture.loadFromFile("C:/Users/Slavkus/Desktop/Morris/images/backimg.jpg");
-	background.setTexture(&texture);*/
+	RenderWindow window(VideoMode(900, 900), "Nine Men's Morris", (Style::Resize,Style::Close));
+	RectangleShape background(Vector2f(800.f,800.f));
+	background.setPosition(50.f, 50.f);
 	Color color(0, 0, 128);
 	background.setFillColor(color);
 
-	VertexArray square(LineStrip, 5);
-	square[0].position = Vector2f(224.f, 168.f);
-	square[1].position = Vector2f(824.f, 168.f);
-	square[2].position = Vector2f(824.f, 568.f);
-	square[3].position = Vector2f(224.f, 568.f);
-	square[4].position = Vector2f(224.f, 168.f);
-
-	square[0].color = Color::Red;
-	square[1].color = Color::Red;
-	square[2].color = Color::Red;
-	square[3].color = Color::Red;
-	square[4].color = Color::Red;
-
+	Gridmaker gridmaker;
 
 	while (window.isOpen())
 	{
@@ -41,7 +26,10 @@ int main()
 
 		window.clear();
 		window.draw(background);
-		window.draw(square);
+		window.draw(gridmaker.generateGridOuter());
+		window.draw(gridmaker.generateGridMid());
+		window.draw(gridmaker.generateGridInner());
+		window.draw(gridmaker.generateGridSlicers());
 		window.display();
 	}
 
